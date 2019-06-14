682134439d188d1c5d7b1f17fcb42985a65426e2
immadisairaj <immadirajendra.sai@gmail.com>
2019-06-14 17:57:00 +0530

    Added files for using retrofit

---
+++ TempleApp/src/main/java/example/TempleApp/classes/pooja/AllPoojaResult.java
@@ -0,0 +1,37 @@
+package example.TempleApp.classes.pooja;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AllPoojaResult {
+
+    @SerializedName("poojas")
+    @Expose
+    private List<Pooja> poojas = null;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public AllPoojaResult() {
+    }
+
+    /**
+     *
+     * @param poojas
+     */
+    public AllPoojaResult(List<Pooja> poojas) {
+        super();
+        this.poojas = poojas;
+    }
+
+    public List<Pooja> getPoojas() {
+        return poojas;
+    }
+
+    public void setPoojas(List<Pooja> poojas) {
+        this.poojas = poojas;
+    }
+
+}
