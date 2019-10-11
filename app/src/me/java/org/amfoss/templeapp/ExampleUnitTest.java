51ec74e46fd529c08d63c4b09d9642ae160784df
likipreetham <likipreethamyellinedi9@gmail.com>
2019-10-12 00:15:15 +0530

    temple

---
+++ app/src/me/java/org/amfoss/templeapp/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package org.amfoss.templeapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** To work on unit tests, switch the Test Artifact in the Build Variants view. */
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
