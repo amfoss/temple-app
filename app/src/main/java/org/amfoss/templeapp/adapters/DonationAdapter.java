e800d2abc581e7f345a02c59a2d1564dc34f1f62
Ajay Prabhakar <48018942+Chromicle@users.noreply.github.com>
2019-12-04 22:24:19 +0530

    feat: fetched Donations (#136)
    * :rocket: fetched pooja details
    
    * :rocket: Add donation
    
    * :rocket: fetch donations
    
    * spotless Applied

---
+++ app/src/main/java/org/amfoss/templeapp/adapters/DonationAdapter.java
@@ -0,0 +1,66 @@
+package org.amfoss.templeapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import org.amfoss.templeapp.R;
+import org.amfoss.templeapp.utils.DonationUtils;
+
+/**
+* @author by Chromicle (ajayprabhakar369@gmail.com)
+* @since 12/4/2019
+*/
+public class DonationAdapter extends RecyclerView.Adapter<DonationAdapter.DonationViewHolder> {
+
+    Context context;
+    List<DonationUtils> DonationList;
+
+    public DonationAdapter(Context context, List<DonationUtils> donationList) {
+        this.context = context;
+        DonationList = donationList;
+    }
+
+    @NonNull
+    @Override
+    public DonationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DonationAdapter.DonationViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.individual_income, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DonationViewHolder holder, int position) {
+
+        DonationUtils donation = DonationList.get(position);
+        holder.listPilgrimName.setText(donation.getPilgrimName());
+        holder.listDonationCause.setText(donation.getDonationCause());
+        holder.listDonationAmount.setText(donation.getDonationAmount());
+        holder.listDonationDate.setText(donation.getDonationDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return DonationList.size();
+    }
+
+    public class DonationViewHolder extends RecyclerView.ViewHolder {
+
+        TextView listPilgrimName;
+        TextView listDonationCause;
+        TextView listDonationDate;
+        TextView listDonationAmount;
+
+        public DonationViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            listPilgrimName = itemView.findViewById(R.id.listPilgrimName);
+            listDonationCause = itemView.findViewById(R.id.listDonationName);
+            listDonationDate = itemView.findViewById(R.id.listDonationDate);
+            listDonationAmount = itemView.findViewById(R.id.listDonationAmount);
+        }
+    }
+}
--- app/src/main/java/org/amfoss/templeapp/fragments/IncomeFragment.java
+++ app/src/main/java/org/amfoss/templeapp/fragments/IncomeFragment.java
@@ -5,10 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 import org.amfoss.templeapp.R;
 import org.amfoss.templeapp.activities.AddDonationActivity;
+import org.amfoss.templeapp.adapters.DonationAdapter;
+import org.amfoss.templeapp.utils.DonationUtils;
+import org.amfoss.templeapp.utils.UserUtils;
 
 /**
 * @author Chromicle (ajayprabhakar369@gmail.com)
@@ -16,7 +36,22 @@ import org.amfoss.templeapp.activities.AddDonationActivity;
 */
 public class IncomeFragment extends Fragment {
 
-    private FloatingActionButton fab;
+    @BindView(R.id.fab_income)
+    FloatingActionButton fab;
+
+    @BindView(R.id.incomeRecyclerView)
+    RecyclerView incomeRecycleView;
+
+    @BindView(R.id.incomeProgressBarLoading)
+    ProgressBar incomeProgressBar;
+
+    @BindView(R.id.textViewAddIncome)
+    TextView textViewAddIncome;
+
+    private List<DonationUtils> donationUtilsArrayList;
+    private DonationAdapter donationAdapter;
+
+    private DatabaseReference donationDb;
 
     public IncomeFragment() {
         // Required empty public constructor
@@ -28,16 +63,62 @@ public class IncomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_income, container, false);
 
-        fab = rootView.findViewById(R.id.fab_income);
-        fab.setOnClickListener(
-                new View.OnClickListener() {
+        ButterKnife.bind(this, rootView);
+
+        addFirebaseInstance();
+
+        fetchDonations();
+        return rootView;
+    }
+
+    private void fetchDonations() {
+
+        incomeRecycleView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        donationUtilsArrayList = new ArrayList<>();
+
+        donationDb.addValueEventListener(
+                new ValueEventListener() {
                     @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(getActivity(), AddDonationActivity.class);
-                        startActivity(intent);
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                            DonationUtils donationValue = postSnapshot.getValue(DonationUtils.class);
+                            donationUtilsArrayList.add(donationValue);
+                        }
+
+                        donationAdapter = new DonationAdapter(getActivity(), donationUtilsArrayList);
+                        incomeRecycleView.setAdapter(donationAdapter);
+                        incomeProgressBar.setVisibility(View.GONE);
+                        changeFabPosition();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
 
-        return rootView;
+    private void addFirebaseInstance() {
+        UserUtils user = new UserUtils();
+        String dbUserName = user.getDbUserName();
+        donationDb = FirebaseDatabase.getInstance().getReference(dbUserName).child("donations");
+    }
+
+    private void changeFabPosition() {
+        RelativeLayout.LayoutParams lay =
+                new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lay.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lay.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        lay.setMargins(2, 2, 75, 75);
+        fab.setLayoutParams(lay);
+        textViewAddIncome.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.fab_income)
+    void setUpFab(View view) {
+        Intent intent = new Intent(getActivity(), AddDonationActivity.class);
+        startActivity(intent);
     }
 }
